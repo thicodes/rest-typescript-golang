79487781a0224ca8bc61e8604d1e7ed6d8ebcab2
thiagoleitedev <thiagoleitebr@gmail.com>
2020-04-14 18:29:12 -0300

    Add init server and routes

--- server-typescript/src/index.ts
+++ server-typescript/src/index.ts
@@ -1,11 +1,27 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { connectDatabase } from './database';
+import { todoGetAll, todoPost, todoDelete } from './modules';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello sds');
-});
+(async () => {
+  try {
+    await connectDatabase();
+  } catch (error) {
+    console.log('Could not connect to database', { error });
+    throw error;
+  }
 
-app.listen(3000, () => {
-  console.log(`Server started`);
-});
+  app.use(cors());
+  app.use(bodyParser.json());
+
+  app.get('/api/todos', todoGetAll);
+  app.post('/api/todos', todoPost);
+  app.delete('/api/todos/:id', todoDelete);
+
+  app.listen(3000, () => {
+    console.log('Server started');
+  });
+})();
