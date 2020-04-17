32ae89d264f8de65bd3d2217c33595eaf5f1086a
thiagoleitedev <thiagoleitebr@gmail.com>
2020-04-17 00:12:46 -0300

    Remove unecessary

--- server-typescript/src/config.ts
+++ server-typescript/src/config.ts
@@ -13,7 +13,5 @@ dotenvSafe.config({
 
 const ENV = process.env;
 
-console.log('ENV >>>', ENV.NODE_ENV);
-
 export const MONGO_URL = ENV.MONGO_URL || 'mongodb://localhost/database';
 export const REST_PORT = ENV.REST_PORT || 5000;
